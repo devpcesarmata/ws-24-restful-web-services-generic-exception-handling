08918d171243ae060894bfb960a8ac909a2153a4
César <dev.pcesarmata@gmail.com>
2020-08-07 17:48:45 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws24restfulwebservicesgenericexceptionhandling/Ws24RestfulWebServicesGenericExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws24restfulwebservicesgenericexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws24RestfulWebServicesGenericExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws24RestfulWebServicesGenericExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws24restfulwebservicesgenericexceptionhandling/Ws24RestfulWebServicesGenericExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws24restfulwebservicesgenericexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws24RestfulWebServicesGenericExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
